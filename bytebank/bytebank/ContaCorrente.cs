40b58494e4983b60225f243fd4da3da060a48118
caiohds <caiohds2020@gmail.com>
2022-08-22 15:39:52 -0300

    Criando objetos

---
+++ bytebank/bytebank/ContaCorrente.cs
@@ -0,0 +1,34 @@
+﻿namespace bytebank
+{
+    public class ContaCorrente
+    {
+        public string titular;
+        public string conta;
+        public int numero_agencia;
+        public string nome_agencia;
+        public double saldo;
+
+        public ContaCorrente(string titular, string conta, int numero_agencia, string nome_agencia, double saldo)
+        {
+            this.titular = titular;
+            this.conta = conta;
+            this.numero_agencia = numero_agencia;
+            this.nome_agencia = nome_agencia;
+            this.saldo = saldo;
+        }
+        public ContaCorrente() { 
+            this.numero_agencia = 0;
+            this.conta = "";
+            this.titular = "";
+            this.nome_agencia = "";
+            this.saldo = 0.0;
+        }
+        public string toString() {
+            return "Titular: " + titular + 
+                   "\nConta: " + conta + 
+                   "\nNúmero da Agência: " + numero_agencia + 
+                   "\nNome da Agência: " + nome_agencia +
+                   "\nSaldo: " + saldo;
+        }
+    }
+}
---
+++ bytebank/bytebank/Program.cs
@@ -0,0 +1,17 @@
+﻿using bytebank;
+Console.WriteLine("Boas Vindas ao seu banco, ByteBank!");
+
+ContaCorrente cc = new ContaCorrente();
+cc.titular = "Caio";
+cc.conta = "10123-x";
+cc.numero_agencia = 12;
+cc.nome_agencia = "Agência central";
+cc.saldo = 100.0;
+
+Console.WriteLine("Titular: " + cc.titular + 
+                  "\nConta: " + cc.conta +
+                  "\nNúmero da Agência: " + cc.numero_agencia + 
+                  "\nNome da Agência: " + cc.nome_agencia +
+                  "\nSaldo: " + cc.saldo);
+
+Console.ReadKey();
