d6f67e733b66ae2701e21004c63732d70c4ceed3
caiohds <caiohds2020@gmail.com>
2022-09-13 15:38:00 -0300

    total de bonificacao e sobrecarga de método

--- bytebank_ADM/bytebank_ADM/Program.cs
+++ bytebank_ADM/bytebank_ADM/Program.cs
@@ -1,14 +1,13 @@
-﻿
-
-using bytebank_ADM.Funcionarios;
+﻿using bytebank_ADM.Funcionarios;
+using bytebank_ADM.Utilitarios;
 
 Console.WriteLine("Boas vindas ao ByteBank Administração.");
-
+GerenciadorBonificacao gerenciador = new GerenciadorBonificacao();
 Funcionario joao = new Funcionario();
 
 joao.Nome = "João";
 joao.Cpf = "12345698";
-joao.Salario = 2000.0;
+joao.Salario = 2000.0; 
 
 Diretor paula = new Diretor();
 paula.Nome = "Paula";
@@ -16,4 +15,8 @@ paula.Cpf = "12345798";
 paula.Salario = 5000.0;
 Console.WriteLine("Bonificação do João: " + joao.getBonificacao());
 Console.WriteLine("Bonificação da Paula: " + paula.getBonificacao());
+gerenciador.Registrar(joao);
+gerenciador.Registrar(paula);
+Console.WriteLine("Total de bonificação: " + gerenciador.getBonificacao());
+
 Console.ReadKey();
