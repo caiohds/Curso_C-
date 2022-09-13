1be6d5255ab616fe89bb60cc9c831d9bf06e6de1
caiohds <caiohds2020@gmail.com>
2022-09-13 14:42:26 -0300

    Criacao da parte do administrador

---
+++ bytebank_ADM/bytebank_ADM/Funcionarios/Funcionario.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bytebank_ADM.Funcionarios
+{
+    public class Funcionario
+    {
+        private string nome;
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+        public double Salario { get; set; }
+
+
+
+        public double getBonificacao()
+        {
+            return Salario * 0.1;
+        }
+    }
+}
---
+++ bytebank_ADM/bytebank_ADM/Program.cs
@@ -0,0 +1,13 @@
+﻿
+
+using bytebank_ADM.Funcionarios;
+
+Console.WriteLine("Boas vindas ao ByteBank Administração.");
+
+Funcionario joao = new Funcionario();
+
+joao.Nome = "João";
+joao.Cpf = "12345698";
+joao.Salario = 2000.0;
+
+Console.WriteLine("Bonificação: " + joao.getBonificacao());
