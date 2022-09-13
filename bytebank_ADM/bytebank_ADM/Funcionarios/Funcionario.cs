b721a79cd4da845dfaf45e5cb52f49209eeb300d
caiohds <caiohds2020@gmail.com>
2022-09-13 14:56:00 -0300

    Criacao do metodo construtor

--- bytebank_ADM/bytebank_ADM/Funcionarios/Funcionario.cs
+++ bytebank_ADM/bytebank_ADM/Funcionarios/Funcionario.cs
@@ -8,16 +8,28 @@ namespace bytebank_ADM.Funcionarios
 {
     public class Funcionario
     {
-        private string nome;
+        //0 - FUNCIONÁRIO COMUM
+        //1 - DIRETOR
+        //2- DESIGNER
+        private int _tipo;
+        public Funcionario(int _tipo)
+        {
+            this._tipo = _tipo;
+        }
         public string Nome { get; set; }
         public string Cpf { get; set; }
         public double Salario { get; set; }
 
+       
 
 
         public double getBonificacao()
         {
-            return Salario * 0.1;
+            if (_tipo == 1)
+            {
+                return Salario; //Caso ele seja direto, recebe 100% de bonificação
+            }
+                return Salario * 0.1; // caso não seja, ele recebe 10%
         }
     }
 }
--- bytebank_ADM/bytebank_ADM/Program.cs
+++ bytebank_ADM/bytebank_ADM/Program.cs
@@ -4,10 +4,11 @@ using bytebank_ADM.Funcionarios;
 
 Console.WriteLine("Boas vindas ao ByteBank Administração.");
 
-Funcionario joao = new Funcionario();
+Funcionario joao = new Funcionario(2);
 
 joao.Nome = "João";
 joao.Cpf = "12345698";
 joao.Salario = 2000.0;
 
 Console.WriteLine("Bonificação: " + joao.getBonificacao());
+Console.ReadKey();
