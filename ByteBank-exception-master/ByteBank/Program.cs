136c55315f4513de44cf08d22b545339a633e165
caiohds <caiohds2020@gmail.com>
2022-09-17 15:33:29 -0300

    Tratando uma exceção com try/catch

--- ByteBank-exception-master/ByteBank/Program.cs
+++ ByteBank-exception-master/ByteBank/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Data.SqlTypes;
+using System.IO;
 using System.Linq;
 using System.Runtime.Remoting.Metadata.W3cXsd2001;
 using System.Text;
@@ -12,42 +14,34 @@ namespace ByteBank
     {
         static void Main(string[] args)
         {
-            ContaCorrente conta1 = new ContaCorrente(7480, 874250);
+            //ContaCorrente conta1 = new ContaCorrente(7480, 874250);
 
             Console.WriteLine(ContaCorrente.TaxaOperacao);
-            metodo(11);
+            metodo(0);
             Console.ReadLine();
             
         }
         private static void metodo(int numero)
         {
-             TestaDivisao(numero);
+            try
+            {
+               TestaDivisao(numero);
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Não é possível dividir um número por 0!");
+            }
+            
+             
         }
         private static void TestaDivisao(int divisor)
         {
             int resultado = dividir(10, divisor);
-            if(resultado == -1)
-            {
-                Console.WriteLine("Não é possível fazer divisão por 0!");
-                return;
-            }
-            if(resultado == -2)
-            {
-                Console.WriteLine("O divisor não pode ser maior que 10!");
-                return;
-            }
+           
             Console.WriteLine("Resultado da divisão de 10 por " + divisor + ": " + resultado);
         }
         private static int dividir(int numero, int divisor)
         {
-            if(divisor == 0)
-            {
-                return -1;
-            }
-            if(divisor > numero)
-            {
-                return -2;
-            }
             return numero / divisor;
         }
     }
