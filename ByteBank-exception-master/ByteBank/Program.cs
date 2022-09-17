2a7bf99f96684770df973461ad885f5747a7c5d2
caiohds <caiohds2020@gmail.com>
2022-09-17 15:54:38 -0300

    Tratando várias exceções

--- ByteBank-exception-master/ByteBank/Program.cs
+++ ByteBank-exception-master/ByteBank/Program.cs
@@ -16,32 +16,42 @@ namespace ByteBank
         {
             //ContaCorrente conta1 = new ContaCorrente(7480, 874250);
 
-            Console.WriteLine(ContaCorrente.TaxaOperacao);
-            metodo(0);
+           // Console.WriteLine(ContaCorrente.TaxaOperacao);
+ 
+            metodo(1);
             Console.ReadLine();
-            
+
         }
         private static void metodo(int numero)
         {
             try
             {
-               TestaDivisao(numero);
+                TestaDivisao(numero);
+
             }
             catch (DivideByZeroException)
             {
                 Console.WriteLine("Não é possível dividir um número por 0!");
             }
-            
-             
+            catch (IOException)
+            {
+                Console.WriteLine("Digite algum valor!");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Digite um valor inteiro!");
+            }
         }
         private static void TestaDivisao(int divisor)
         {
             int resultado = dividir(10, divisor);
-           
+
             Console.WriteLine("Resultado da divisão de 10 por " + divisor + ": " + resultado);
         }
         private static int dividir(int numero, int divisor)
         {
+
+
             return numero / divisor;
         }
     }
